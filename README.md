33d2e09144b7fc0fde5e0349e1c51179f08eb257
Manuel García <solipa@gmail.com>
2015-08-22 22:01:26 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Shareaholic plugin for b2evolution
+
+![Shareaholic Plugin](pluginshot.png)
+
+Using this plugin you can display the Shareaholic sharing bar into the contents of your site and let your visitors share them to their preferred social networks and websites.
+
+### Installation
+
+You can use this plugin in your site by following the regular plugins installation process detailed in this manual page: [Plugins available for installation](http://b2evolution.net/man/plugins-available-for-installation).
+
+After having successfully installed the plugin in your site, you need to configure a **Shareaholic** account in order to get a `Pub Key`, which is a code with the following format: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX.
+
+1. Go to the [Shareaholic website](http://www.shareaholic.com/), register a new user account (if you have not done it yet) and login into the site.
+2. Go to the **Website Dashboard** and add a new website. You will get a `Site ID`, a string of 32 alphadecimal characters. ![Website Dashboard](screenshots/shareaholic-settings_01.png)
+3. Scroll down into the **App Locations** section and add a new one, configuring the layout and services that you want to display in your site. You will get a `Location APP ID`. ![App Location settings](screenshots/shareaholic-settings_02.png)
+4. Go back to your b2evolution site and open the plugins settings page of the collection in which you want to display the Shareaholic widget: [Plugin Settings](http://b2evolution.net/man/blog-plugin-settings)
+5. Enable the plugin for that collection and paste the `Site ID` and `Location APP ID`. ![Plugin settings](screenshots/shareaholic-blog_plugins_settings.png)
+
+As this is a **Text Renderer plugin**, you may control when to enable / disable it for individual posts by checking the **Shareaholic** option in the **Text Renderers** panel of the posts' edition form. ![Text renderers panel](screenshots/shareaholic-text_renderers_panel.png)
---
+++ addons/shareaholic/shareaholic.class.php
@@ -0,0 +1,84 @@
+<?php
+
+class socialshare_shareaholic extends shareaholic_pluginAddOn
+{
+	var $addon_name = 'shareaholic';
+
+	/** 
+	 * Get current collection settings
+	 */
+	static function get_coll_setting_definitions()
+	{
+		return array(
+			'shareaholic_site_id' => array(
+				'label' => T_('Site ID'),
+				'size' => 70,
+				'defaultvalue' => '',
+				'note' => T_('The ID that you get from your social sharing service.'),
+			),
+			'shareaholic_applocation_app_id' => array(
+				'label' => T_('Location APP ID'),
+				'size' => 70,
+				'defaultvalue' => '',
+				'note' => T_('The Id of the location created for your site in the Shareaholic\'s Dashboard. See documentation for details.'),
+			),
+		);
+	}
+
+	/** Misc methods **/
+
+	/**
+	 * Inserts required html markup
+	 */
+	//TODO: allow per post-type inclusion
+	function insert_code_block( & $params )
+	{
+		$content = & $params['data'];
+		$item = & $params['Item'];
+		
+		$content .= "\n"
+					.'<div class="shareaholic-canvas" data-app="share_buttons" data-app-id="' . $this->coll_settings['shareaholic_applocation_app_id'] . '"></div>'
+					."\n";
+	}
+
+	/** Plugin HOOKS **/
+
+	function SkinBeginHtmlHead( & $params )
+	{
+		global $Blog;
+
+		$script = "
+//<![CDATA[
+  (function() {
+    var shr = document.createElement('script');
+    shr.setAttribute('data-cfasync', 'false');
+    shr.src = '//dsms0mj1bbhn4.cloudfront.net/assets/pub/shareaholic.js';
+    shr.type = 'text/javascript'; shr.async = 'true';
+    shr.onload = shr.onreadystatechange = function() {
+      var rs = this.readyState;
+      if (rs && rs != 'complete' && rs != 'loaded') return;
+      var site_id = '".$this->plugin->get_coll_setting( 'shareaholic_site_id', $Blog )."';
+      try { Shareaholic.init(site_id); } catch (e) {}
+    };
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(shr, s);
+  })();
+//]]>";
+
+			add_js_headline($script);
+	}
+
+	function RenderItemAsHtml( & $params )
+	{
+		if( ! empty($this->coll_settings['shareaholic_applocation_app_id']) )
+		{
+			$this->insert_code_block( $params );
+			return true;	
+		}
+		else
+		{ // Unknown call, Don't render this case
+			return;
+		}
+		
+	}
+}
